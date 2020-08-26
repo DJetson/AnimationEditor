7dd0d8df1c8e4e3bad8eae13745e6949b588e8a8
djetson <djetson@gmail.com>
2020-08-25 20:01:42 -0700

    Added animation playback.
    -Playback speed is fixed at 24 frames per second at the moment. Eventually that will be editable
    
    -Not totally sure about the pattern that I'm using to take care of animation playback, but we'll just see what happens.

---
+++ AnimationEditor/Utilities/AnimationPlaybackState.cs
@@ -0,0 +1,82 @@
+﻿using AnimationEditor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace AnimationEditor.Utilities
+{
+    public class AnimationPlaybackState
+    {
+        public enum PlaybackStates { Stop, Play, Pause };
+
+        private AnimationTimelineViewModel _AnimationTimelineViewModel;
+        private int _OriginalSelectedFrameIndex;
+        private DispatcherTimer _PlaybackTimer;
+        private int _CurrentFrameIndex;
+        private FrameViewModel _CurrentFrame;
+        private List<FrameViewModel> _Frames;
+        private int _FrameIndexOffset;
+        private double _FramesPerSecond;
+
+        private PlaybackStates _CurrentState = PlaybackStates.Stop;
+        public PlaybackStates PlaybackState => _CurrentState;
+
+        public delegate void AnimationPlaybackStateChangeRequestedEvent(object requestedBy, PlaybackStates requestedState);
+        private event AnimationPlaybackStateChangeRequestedEvent PlaybackStateChangeRequested;
+
+        public AnimationPlaybackState(AnimationTimelineViewModel animationTimelineViewModel, List<FrameViewModel> playbackFrames = null)
+        {
+            _AnimationTimelineViewModel = animationTimelineViewModel;
+            _OriginalSelectedFrameIndex = animationTimelineViewModel.Frames.IndexOf(animationTimelineViewModel.SelectedFrame);
+
+            if (playbackFrames != null)
+            {
+                _FrameIndexOffset = animationTimelineViewModel.Frames.IndexOf(playbackFrames.FirstOrDefault());
+                _Frames = playbackFrames;
+            }
+            else
+            {
+                _FrameIndexOffset = 0;
+                _Frames = animationTimelineViewModel.Frames.ToList();
+            }
+
+            _FramesPerSecond = animationTimelineViewModel.FramesPerSecond;
+
+            _PlaybackTimer = new DispatcherTimer()
+            {
+                Interval = new TimeSpan((int)(TimeSpan.TicksPerSecond * (1.0 / animationTimelineViewModel.FramesPerSecond)))
+            };
+
+            _PlaybackTimer.Tick += (sender, e) =>
+            {
+                _CurrentFrameIndex = (_CurrentFrameIndex + 1) % _Frames.Count;
+                _AnimationTimelineViewModel.SelectedFrame = _AnimationTimelineViewModel.Frames[_CurrentFrameIndex + _FrameIndexOffset];
+            };
+
+            PlaybackStateChangeRequested += (requestedBy, state) =>
+            {
+                _PlaybackTimer.Stop();
+
+                if (state == PlaybackStates.Stop)
+                {
+                    _AnimationTimelineViewModel.SelectedFrame = _AnimationTimelineViewModel.Frames[_OriginalSelectedFrameIndex];
+                    _CurrentState = PlaybackStates.Stop;
+                }
+                else if(state == PlaybackStates.Pause)
+                {
+                    _CurrentState = PlaybackStates.Pause;
+                }
+            };
+
+            _CurrentState = PlaybackStates.Play;
+            _AnimationTimelineViewModel.IsInPlaybackMode = true;
+            _PlaybackTimer.Start();
+        }
+
+        public void RequestPlaybackState(object requestedBy, PlaybackStates state)
+            => PlaybackStateChangeRequested?.Invoke(requestedBy, state);
+    }
+}
--- AnimationEditor/ViewModels/AnimationTimelineViewModel.cs
+++ AnimationEditor/ViewModels/AnimationTimelineViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using AnimationEditor.BaseClasses;
 using AnimationEditor.Interfaces;
+using AnimationEditor.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Ink;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace AnimationEditor.ViewModels
 {
@@ -41,11 +45,128 @@ namespace AnimationEditor.ViewModels
             }
         }
 
+        private double _FramesPerSecond = 24;
+        public double FramesPerSecond
+        {
+            get { return _FramesPerSecond; }
+            set { _FramesPerSecond = value; NotifyPropertyChanged(); }
+        }
+
         private DelegateCommand _AppendFrame;
         public DelegateCommand AppendFrame
         {
             get => _AppendFrame;
             set { _AppendFrame = value; NotifyPropertyChanged(); }
+        }
+
+        public void AppendFrame_Execute(object parameter)
+        {
+            var newFrame = new FrameViewModel();
+            AddFrameAtIndex(newFrame, Frames.Count);
+            SelectedFrame = newFrame;
+        }
+
+        public bool AppendFrame_CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        private DelegateCommand _PlayAnimation;
+        public DelegateCommand PlayAnimation
+        {
+            get { return _PlayAnimation; }
+            set { _PlayAnimation = value; NotifyPropertyChanged(); }
+        }
+
+        private DelegateCommand _StopAnimation;
+        public DelegateCommand StopAnimation
+        {
+            get { return _StopAnimation; }
+            set { _StopAnimation = value; NotifyPropertyChanged(); }
+        }
+
+        private bool _IsInPlaybackMode = false;
+        public bool IsInPlaybackMode
+        {
+            get { return _IsInPlaybackMode; }
+            set { _IsInPlaybackMode = value; NotifyPropertyChanged(); }
+        }
+
+        private AnimationPlaybackState _PlaybackState;
+
+        public void PlayAnimation_Execute(object parameter)
+        {
+            //TODO: Turn off onionskins. This will require a minor refactor so that the binding used for onionskin
+            //      visibility is an AND between some flag that gets raised by this method, and the actual visibility
+            //      setting. So that it turns off when the animation is playing and turns back on when it stops.
+
+            //NOTE: I may have to eventually do some caching up top here. Rasterize all of the InkCanvases in the series
+            //      and just flip through those instead of directly showing the InkCanvases. It will depend entirely on
+            //      how well it performs in it's initial, most basic form.
+
+            if (IsInPlaybackMode == false)
+            {
+                _PlaybackState = new AnimationPlaybackState(this);
+            }
+            else
+            {
+                _PlaybackState.RequestPlaybackState(this, AnimationPlaybackState.PlaybackStates.Pause);
+                IsInPlaybackMode = false;
+            }
+
+            //Timer t = new Timer(AnimationTimerIntervalElapsed,null,0, (int)(1000 * (1.0/FramesPerSecond)));
+
+        }
+        public void StopAnimation_Execute(object parameter)
+        {
+            _PlaybackState.RequestPlaybackState(this, AnimationPlaybackState.PlaybackStates.Stop);
+
+            if (_PlaybackState.PlaybackState == AnimationPlaybackState.PlaybackStates.Stop)
+                _PlaybackState = null;
+
+            IsInPlaybackMode = false;
+        }
+
+        //private void PauseAnimationPlayback()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //private void BeginAnimationPlayback()
+        //{
+        //    DispatcherTimer timer = new DispatcherTimer();
+        //    timer.Interval = new TimeSpan((int)(1000 * (1.0 / FramesPerSecond)));
+
+        //    int selectedFrameIndex = Frames.IndexOf(SelectedFrame);
+
+        //    timer.Tick += (sender, e) =>
+        //    {
+        //        int nextFrameIndex = (selectedFrameIndex + 1) % Frames.Count;
+
+        //        SelectedFrame = Frames[nextFrameIndex];
+        //    };
+
+        //    IsInPlaybackMode = true;
+        //    timer.Start();
+        //}
+
+        //private void AnimationTimerIntervalElapsed(object state)
+        //{
+        //}
+
+        public bool PlayAnimation_CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public bool StopAnimation_CanExecute(object parameter)
+        {
+            if(_PlaybackState == null)
+                return false;
+            if (_PlaybackState.PlaybackState != AnimationPlaybackState.PlaybackStates.Play)
+                return false;
+
+            return true;
         }
 
         //private ObservableCollection<FrameViewModel> _NextFrames = new ObservableCollection<FrameViewModel>();
@@ -101,6 +222,8 @@ namespace AnimationEditor.ViewModels
         public void InitializeCommands()
         {
             AppendFrame = new DelegateCommand(AppendFrame_CanExecute, AppendFrame_Execute);
+            PlayAnimation = new DelegateCommand(PlayAnimation_CanExecute, PlayAnimation_Execute);
+            StopAnimation = new DelegateCommand(StopAnimation_CanExecute, StopAnimation_Execute);
         }
 
         public AnimationTimelineViewModel(List<IFrameModel> frames)
@@ -124,18 +247,6 @@ namespace AnimationEditor.ViewModels
             Frames.Add(new FrameViewModel());
             SelectedFrame = Frames.FirstOrDefault();
 
-        }
-
-        public void AppendFrame_Execute(object parameter)
-        {
-            var newFrame = new FrameViewModel();
-            AddFrameAtIndex(newFrame, Frames.Count);
-            SelectedFrame = newFrame;
-        }
-
-        public bool AppendFrame_CanExecute(object parameter)
-        {
-            return true;
         }
 
         public void AddFrameAtIndex(FrameViewModel frame, int index)
