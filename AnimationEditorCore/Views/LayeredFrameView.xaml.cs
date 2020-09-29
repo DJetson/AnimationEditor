daa1a685a9124a17b8d59b38fbc806204e2fd50f
djetson <djetson@gmail.com>
2020-09-29 00:54:03 -0700

    Refactored some of the LayerCanvasView into a new LayeredFrameView
    -This was done in a manner very much like the OnionSkinView. The existing markup was moved into its own control and a unique dependency property was defined on the new control so that the collection of FrameViewModels that comprise a complete frame are passed in as a collection of frame layers. Other than that the markup is essentially unchanged.

---
+++ AnimationEditorCore/Views/LayeredFrameView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AnimationEditorCore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimationEditorCore.Views
+{
+    /// <summary>
+    /// Interaction logic for LayeredFrameView.xaml
+    /// </summary>
+    public partial class LayeredFrameView : UserControl
+    {
+        private static readonly DependencyProperty FrameLayersProperty = DependencyProperty.Register("FrameLayers", typeof(ObservableCollection<FrameViewModel>), typeof(LayeredFrameView), new FrameworkPropertyMetadata(new ObservableCollection<FrameViewModel>()));
+        public ObservableCollection<FrameViewModel> FrameLayers
+        {
+            get => (ObservableCollection<FrameViewModel>)GetValue(FrameLayersProperty);
+            set { SetValue(FrameLayersProperty, (ObservableCollection<FrameViewModel>)value); }
+        }
+
+        public LayeredFrameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
