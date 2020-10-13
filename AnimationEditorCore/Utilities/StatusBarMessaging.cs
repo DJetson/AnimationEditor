6562f101da7106691486fc851ec6e2d0dadeb7d7
djetson <djetson@gmail.com>
2020-10-13 02:26:45 -0700

    Added a rough first draft of a StatusBar
    -This thing relies on some pretty intense WPF stuff to actually work.
    
    -This whole system is predicated on Interaction Triggers to function, and since Interaction Triggers can't be added to a style, I had to modify the ImageButton and ImageToggleButton styles that are used by the toolbar buttons so that they now have a ContentControl wrapping the Image Rectangle. The interaction triggers are attached to the ContentControl and they get triggered by MouseEnter and MouseLeave events.
    
    -On MouseEnter they execute the SetStatusMessageCommand provided that the CommandParameter is a Button, ToggleButton or RadioButton control. If those conditions are met, it peels off the Command, casts it as a RequeryBase and grabs the Description property. Then it calls SetStatusMessage. which is a static method on the StatusBarMessaging singleton. This sets the CurrentMessage property of same which is the binding source for the StatusBar controls sole and only StatusBarItem.
    
    -On MouseLeave, the entire process happens exactly the same way except that it calls the ClearStatusMessageCommand which, you guessed it, calls the ClearStatusMessage method on the StatusBarMessaging singleton.
    
    -The end result is that any command bound to any toggle, radio, or regular button which has a Description string automatically pumps that string into the status bar whenever the mouse is over the button that the command is bound to.
    
    -It would be great to add another property to RequeryBase that holds the Key Gesture that a particular command is triggered by so that the Key Gesture can also be pumped into the status bar, along with, optionally, any alternative behavior that can be triggered by a different combination of modifier keys.
    
    -Lastly, the two commands I mentioned, SetStatusBarMessageCommand and ClearStatusBarMessageCommand are very messy and could do for some serious refactoring. Because of the way they were originally written, they crashed any time they were triggered by a MouseEnter or MouseLeave event raised by a Radio or Toggle button type. So, because it's late and I'm very tired, I just sort of brute forced a solution, and it shows.

---
+++ AnimationEditorCore/Utilities/StatusBarMessaging.cs
@@ -0,0 +1,38 @@
+﻿using AnimationEditorCore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimationEditorCore.Utilities
+{
+    public class StatusBarMessaging : ViewModelBase
+    {
+        private static StatusBarMessaging _Instance = null;
+        public static StatusBarMessaging Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                    _Instance = new StatusBarMessaging();
+                return _Instance;
+            }
+        }
+
+        private string _CurrentMessage;
+        public string CurrentMessage
+        {
+            get { return _CurrentMessage; }
+            set { _CurrentMessage = value; NotifyPropertyChanged(); }
+        }
+
+        public static void ClearStatusBarMessage()
+        {
+            Instance.CurrentMessage = String.Empty;
+        }
+
+        public static void SetStatusBarMessage(string message)
+        {
+            Instance.CurrentMessage = message;
+        }
+    }
+}
