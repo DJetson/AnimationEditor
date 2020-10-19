a603bc3e816c1d6aac449cdf31e04e30ed07d789
djetson <djetson@gmail.com>
2020-10-18 17:45:29 -0700

    Wrote some tests for TimelineNavigation and Playback

---
+++ AnimationEditor.Tests/ViewModels/AnimationPlaybackViewModelTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimationEditorCore.ViewModels
+{
+    [TestClass]
+    public class AnimationPlaybackViewModelTest
+    {
+        [TestMethod]
+        [DeploymentItem("DeploymentItems/TestFile.anws")]
+        public void StartPlayback()
+        {
+            var workspaceManager = new WorkspaceManagerViewModel();
+
+            var openCommand = new Commands.Workspace.OpenWorkspaceCommand();
+            Assert.IsTrue(openCommand.CanExecute(workspaceManager));
+
+            openCommand.OpenWorkspaceFile("TestFile.anws", workspaceManager);
+            Assert.AreEqual("TestFile", workspaceManager.SelectedWorkspace.DisplayName, "selected workspace display name");
+
+            var workspace = workspaceManager.SelectedWorkspace;
+            var timeline = workspace.TimelineViewModel;
+            var playback = timeline.AnimationPlaybackViewModel;
+
+            //Assert.IsFalse(playback.IsPlaybackActive, "Incorrect playback state");
+
+            var startPlaybackCommand = new Commands.Timeline.Playback.TogglePlaybackCommand();
+
+            Assert.IsTrue(startPlaybackCommand.CanExecute(timeline));
+
+            startPlaybackCommand.Execute(timeline);
+
+            Assert.IsTrue(playback.IsPlaybackActive);
+        }
+
+        [TestMethod]
+        [DeploymentItem("DeploymentItems/TestFile.anws")]
+        public void StopPlayback()
+        {
+            var workspaceManager = new WorkspaceManagerViewModel();
+
+            var openCommand = new Commands.Workspace.OpenWorkspaceCommand();
+            Assert.IsTrue(openCommand.CanExecute(workspaceManager));
+
+            openCommand.OpenWorkspaceFile("TestFile.anws", workspaceManager);
+            Assert.AreEqual("TestFile", workspaceManager.SelectedWorkspace.DisplayName, "selected workspace display name");
+
+            var workspace = workspaceManager.SelectedWorkspace;
+            var timeline = workspace.TimelineViewModel;
+            var playback = timeline.AnimationPlaybackViewModel;
+
+            //Assert.IsFalse(playback.IsPlaybackActive, "Incorrect playback state");
+
+            var StopPlaybackCommand = new Commands.Timeline.Playback.StopPlaybackCommand();
+            var startPlaybackCommand = new Commands.Timeline.Playback.TogglePlaybackCommand();
+
+            Assert.IsTrue(startPlaybackCommand.CanExecute(timeline));
+
+            startPlaybackCommand.Execute(timeline);
+
+            Assert.IsTrue(playback.IsPlaybackActive);
+
+            Assert.IsTrue(StopPlaybackCommand.CanExecute(timeline));
+
+            StopPlaybackCommand.Execute(timeline);
+
+            Assert.AreEqual(1, timeline.SelectedFrameIndex);
+            //Assert.IsTrue(playback.IsPlaybackActive);
+        }
+    }
+}
--- AnimationEditor.Tests/ViewModels/TimelineNavigationTest.cs
+++ AnimationEditor.Tests/ViewModels/TimelineNavigationTest.cs
@@ -10,7 +10,7 @@ namespace AnimationEditorCore.ViewModels
     {
         [TestMethod]
         [DeploymentItem("DeploymentItems/TestFile.anws")]
-        public void NavigateToFirstLastFrame()
+        public void NavigateToFirstFrame()
         {
             var workspaceManager = new WorkspaceManagerViewModel();
 
@@ -20,8 +20,84 @@ namespace AnimationEditorCore.ViewModels
             openCommand.OpenWorkspaceFile("TestFile.anws", workspaceManager);
             Assert.AreEqual("TestFile", workspaceManager.SelectedWorkspace.DisplayName, "selected workspace display name");
 
+            var workspace = workspaceManager.SelectedWorkspace;
+            var timeline = workspace.TimelineViewModel;
+            Assert.AreEqual(1, timeline.SelectedFrameIndex, "The TestFile did not have the correct SelectedFrameIndex");
 
+            var navigateToFirstFrameCommand = new Commands.Timeline.Navigation.NavigateToFirstFrameCommand();
+            Assert.IsTrue(navigateToFirstFrameCommand.CanExecute(timeline));
+
+            navigateToFirstFrameCommand.Execute(timeline);
+            Assert.AreEqual(0, timeline.SelectedFrameIndex, "NavigateToFirstFrameCommand failed to navigate to frame 0");
         }
 
+        [TestMethod]
+        [DeploymentItem("DeploymentItems/TestFile.anws")]
+        public void NavigateToLastFrame()
+        {
+            var workspaceManager = new WorkspaceManagerViewModel();
+
+            var openCommand = new Commands.Workspace.OpenWorkspaceCommand();
+            Assert.IsTrue(openCommand.CanExecute(workspaceManager));
+
+            openCommand.OpenWorkspaceFile("TestFile.anws", workspaceManager);
+            Assert.AreEqual("TestFile", workspaceManager.SelectedWorkspace.DisplayName, "selected workspace display name");
+
+            var workspace = workspaceManager.SelectedWorkspace;
+            var timeline = workspace.TimelineViewModel;
+            Assert.AreEqual(1, timeline.SelectedFrameIndex, "The TestFile did not have the correct SelectedFrameIndex");
+
+            var navigateToLastFrameCommand = new Commands.Timeline.Navigation.NavigateToLastFrameCommand();
+            Assert.IsTrue(navigateToLastFrameCommand.CanExecute(timeline));
+
+            navigateToLastFrameCommand.Execute(timeline);
+            Assert.AreEqual(4, timeline.SelectedFrameIndex, "NavigateToLastFrameCommand failed to navigate to the last frame index");
+        }
+
+        [TestMethod]
+        [DeploymentItem("DeploymentItems/TestFile.anws")]
+        public void NavigateToPreviousFrame()
+        {
+            var workspaceManager = new WorkspaceManagerViewModel();
+
+            var openCommand = new Commands.Workspace.OpenWorkspaceCommand();
+            Assert.IsTrue(openCommand.CanExecute(workspaceManager));
+
+            openCommand.OpenWorkspaceFile("TestFile.anws", workspaceManager);
+            Assert.AreEqual("TestFile", workspaceManager.SelectedWorkspace.DisplayName, "selected workspace display name");
+
+            var workspace = workspaceManager.SelectedWorkspace;
+            var timeline = workspace.TimelineViewModel;
+            Assert.AreEqual(1, timeline.SelectedFrameIndex, "The TestFile did not have the correct SelectedFrameIndex");
+
+            var navigateToPreviousFrameCommand = new Commands.Timeline.Navigation.NavigateToPreviousFrameCommand();
+            Assert.IsTrue(navigateToPreviousFrameCommand.CanExecute(timeline));
+
+            navigateToPreviousFrameCommand.Execute(timeline);
+            Assert.AreEqual(0, timeline.SelectedFrameIndex, "NavigateToPreviousFrameCommand failed to navigate to the Previous frame index");
+        }
+
+        [TestMethod]
+        [DeploymentItem("DeploymentItems/TestFile.anws")]
+        public void NavigateToNextFrame()
+        {
+            var workspaceManager = new WorkspaceManagerViewModel();
+
+            var openCommand = new Commands.Workspace.OpenWorkspaceCommand();
+            Assert.IsTrue(openCommand.CanExecute(workspaceManager));
+
+            openCommand.OpenWorkspaceFile("TestFile.anws", workspaceManager);
+            Assert.AreEqual("TestFile", workspaceManager.SelectedWorkspace.DisplayName, "selected workspace display name");
+
+            var workspace = workspaceManager.SelectedWorkspace;
+            var timeline = workspace.TimelineViewModel;
+            Assert.AreEqual(1, timeline.SelectedFrameIndex, "The TestFile did not have the correct SelectedFrameIndex");
+
+            var navigateToNextFrameCommand = new Commands.Timeline.Navigation.NavigateToNextFrameCommand();
+            Assert.IsTrue(navigateToNextFrameCommand.CanExecute(timeline));
+
+            navigateToNextFrameCommand.Execute(timeline);
+            Assert.AreEqual(2, timeline.SelectedFrameIndex, "NavigateToNextFrameCommand failed to navigate to the Next frame index");
+        }
     }
 }
